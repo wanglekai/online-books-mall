5297bc1b192a3aaf5720b9ff719cb55a4e302a0a
wanglekai <xiaoguailele@foxmail.com>
2021-08-09 23:56:20 +0800

    global store init test

--- src/components/core/Home.js
+++ src/components/core/Home.js
@@ -1,9 +1,11 @@
+import { useSelector } from "react-redux";
 import Layout from "./Layout";
 
 function Home () {
+  const state = useSelector(state => state)
   return (
     <Layout>
-      Home
+      Home {JSON.stringify(state)}
     </Layout>
   )
 }
