597323c872636ec13a6a3430d337a455ac42d167
wanglekai <xiaoguailele@foxmail.com>
2021-08-14 20:52:03 +0800

    创建导航菜单

--- src/components/core/Layout.js
+++ src/components/core/Layout.js
@@ -1,7 +1,19 @@
+import { Menu } from 'antd';
+import { useSelector } from 'react-redux'
 import React from 'react';
+import { Link } from 'react-router-dom';
 function Layout ({children}) {
+  const router = useSelector(state => state.router)
   return (
     <div>
+      <Menu mode="horizontal" selectedKeys={[router.location.pathname]}>
+        <Menu.Item key="/">
+          <Link to="/">首页</Link>
+        </Menu.Item>
+        <Menu.Item key="/shop">
+          <Link to="/shop">商城</Link>
+        </Menu.Item>
+      </Menu>
       Layout {children}
     </div>
   )
